73898fc2a0f9b958c3aab49f548df6a65cba32d2
yang <545382026@qq.com>
2019-04-16 15:32:30 +0800

    创建数据库，添加内容

--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -3,6 +3,22 @@ from django.db import models
 # Create your models here.
 
 
+class Bookinfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_data = models.DateTimeField(auto_now=True)
+
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=50)
+    # 外键 第一个参数为表名 第二个参数代表类型
+    hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE)
+
+
+
+
+
 """
  django MVT  M
  ORM 对象中O
