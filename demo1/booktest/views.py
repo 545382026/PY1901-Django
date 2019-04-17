d38b7f5c61fef5bd3eab27758b65a384c495f5bc
yang <545382026@qq.com>
2019-04-17 21:29:47 +0800

    去除硬编码

--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Bookinfo,HeroInfo
+from django.template import loader
 # Create your views here.
 
 # 定义视图函数
 def index(request):
-    return HttpResponse('首页')
+    # return HttpResponse('首页')
+    # 加载模板
+    # indextem = loader.get_template('booktest/index.html')
+    # # 使用变量渲染模板
+    con = {'username':'yang teng'}
+    # result = indextem.render(con)
+    # # 返回模板
+    # return HttpResponse(result)
+    # 快捷方式
+    return render(request,'booktest/index.html',con)
 
 def list(requset):
-    return HttpResponse('列表页')
+    book = Bookinfo.objects.all()
+    return render(requset,'booktest/list.html',{'booklist':book})
 
-def detail(request, id):
-    try:
-        book = Bookinfo.objects.get(pk=int(id))
-        # book = Bookinfo.objects.all()[int(id)].btitle
-        return HttpResponse(book)
-    except:
-        return HttpResponse('请输入正确id')
+def detail(request,id):
+    # try:
+    #     # book = Bookinfo.objects.get(pk=int(id))
+    #     # book = Bookinfo.objects.all()[int(id)].btitle
+    #     # return HttpResponse(book)
+    # except:
+    #     return HttpResponse('错误id')
+    book = Bookinfo.objects.get(pk=int(id))
+    return render(request, 'booktest/detail.html', {'booklist':book})
 
 
 """
