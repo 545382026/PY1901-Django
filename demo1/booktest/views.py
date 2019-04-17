e736f25ece137e1f03312f167112547f3413db3d
yang <545382026@qq.com>
2019-04-17 15:14:25 +0800

    视图传参

--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Bookinfo,HeroInfo
 # Create your views here.
 
 # 定义视图函数
@@ -9,7 +10,13 @@ def index(request):
 def list(requset):
     return HttpResponse('列表页')
 
-
+def detail(request, id):
+    try:
+        book = Bookinfo.objects.get(pk=int(id))
+        # book = Bookinfo.objects.all()[int(id)].btitle
+        return HttpResponse(book)
+    except:
+        return HttpResponse('请输入正确id')
 
 
 """
